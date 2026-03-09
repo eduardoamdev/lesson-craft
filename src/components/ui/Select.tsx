262753870b2bfadff78975e99158816bb062d99f
eduardoamdev <eduardoamdev@gmail.com>
2026-03-09 22:22:51 +0400

    Hydration problems solved in select componenet

--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -38,22 +38,11 @@ export default function Select({
             </option>
           ))}
         </select>
-        <div className="absolute right-6 top-1/2 -translate-y-1/2 pointer-events-none text-[#475569]">
-          <svg
-            width="12"
-            height="8"
-            viewBox="0 0 12 8"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M1 1.5L6 6.5L11 1.5"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
+        <div
+          className="absolute right-6 top-1/2 -translate-y-1/2 pointer-events-none text-[#475569] text-xl"
+          style={{ WebkitTextStroke: "0.5px currentColor" }}
+        >
+          ⌄
         </div>
       </div>
     </div>
